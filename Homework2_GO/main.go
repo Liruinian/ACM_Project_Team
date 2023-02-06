c574f1f6e77eb354613e2f94d177207d18a39799
Tim_LRN2016 <2941330150@qq.com>
2023-02-06 16:11:55 +0800

    golang added

--- Homework2_GO/main.go
+++ Homework2_GO/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
 )
 
 func main() {
-
+	database, err := sqlx.Open("mysql", "root:localhost@tcp(127.0.0.1:3306)/mytest")
+	if err != nil {
+		fmt.Println("open mysql failed,", err)
+		return
+	}
+	fmt.Println(database)
 	ginServer := gin.Default()
 	ginServer.POST("/login", func(context *gin.Context) {
-		context.JSON()
+		context.JSON(200, nil)
 	})
 
 	ginServer.GET("/hello", func(context *gin.Context) {
