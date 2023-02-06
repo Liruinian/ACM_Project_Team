d655b181507109533258f70ccea68e770d15a288
Tim_LRN2016 <2941330150@qq.com>
2023-02-06 16:04:34 +0800

    golang added

--- Homework2_GO/main.go
+++ Homework2_GO/main.go
@@ -7,6 +7,10 @@ import (
 func main() {
 
 	ginServer := gin.Default()
+	ginServer.POST("/login", func(context *gin.Context) {
+		context.JSON()
+	})
+
 	ginServer.GET("/hello", func(context *gin.Context) {
 		context.JSON(200, gin.H{"msg": "hello,gin"})
 	})
